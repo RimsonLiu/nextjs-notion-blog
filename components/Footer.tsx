e1458f4aa207c6e2596e7dd32d72ff5a5dd8b456
rimsonliu <rimsonliu@tencent.com>
2022-02-11 13:46:59 +0800

    删除访问量

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -40,7 +40,6 @@ export const Footer: React.FC<{
         </div>
       ) : null}
 
-<div className="bsz"><span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span></div>
     </footer>
   )
 }
